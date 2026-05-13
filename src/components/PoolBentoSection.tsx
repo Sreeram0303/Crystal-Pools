12fed28ff61bacf68474ad51b1bdda13e02c12d5
Sreeram0303 <chavalisaisreeramyadav@gmail.com>
2026-05-13 21:35:15 +0530

    Images Fix3

--- src/components/PoolBentoSection.tsx
+++ src/components/PoolBentoSection.tsx
@@ -32,25 +32,25 @@ const POOL_TYPES = [
     id: 'vanishing-edge-swimming-pools',
     title: 'VANISHING EDGE SWIMMING POOLS',
     desc: 'Vanishing edge pool is a swimming or reflecting pool that produces a visual effect of water extending to the horizon. Perfect for scenic locations.',
-    img: '/images/pool-types/vanishing-edge.jpg'
+    img: '/images/pool-types/vanishing-edge.png'
   },
   {
     id: 'overflow-type-swimming-pools',
     title: 'OVERFLOW TYPE SWIMMING POOLS',
     desc: 'A perimeter-overflow pool is a type of vanishing-edge pool designed so that water spills over all four walls, creating a flawless mirror surface.',
-    img: '/images/pool-types/overflow.jpg'
+    img: '/images/pool-types/overflow.png'
   },
   {
     id: 'skimmer-type-swimming-pools',
     title: 'SKIMMER TYPE SWIMMING POOLS',
     desc: 'A skimmer swimming pool is designed to pull water into the system from the pools surface with a skimming action. Highly efficient and cost-effective.',
-    img: '/images/pool-types/skimmer.jpg'
+    img: '/images/pool-types/skimmer.png'
   },
   {
     id: 'readymade-swimming-pool',
     title: 'READYMADE SWIMMING POOL',
     desc: 'Prefabs and readymade pools offer quick installation. Our service department is available for all equipment repair and replace as well as new readymade installs.',
-    img: '/images/pool-types/readymade.jpg'
+    img: '/images/pool-types/readymade.png'
   }
 ];
 
